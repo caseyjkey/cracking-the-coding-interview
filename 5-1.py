755ff7269e0db474c7908ff5f5d70dd73480b074
Casey Key <casey.key@pm.me>
2024-05-11 23:50:40 -0700

    added bit manipulation exercise

---
+++ 5-1.py
@@ -0,0 +1,5 @@
+# Insertion
+# Given two 32 bit numbers N and M, write a method to instert M into N given two positions i and j
+# Example, N = 10000000000 M = 10011 i = 2 j = 6
+# Output: N = 10001001100
+# Assume M can fit between i and j
