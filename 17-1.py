9b1ebf01ddebad083f5ae3038f962c71886ff48b
Casey Key <casey.key@pm.me>
2024-06-02 22:31:48 -0700

    Added solution

--- 17-1.py
+++ 17-1.py
@@ -1,2 +1,11 @@
 # Write a function to add without using + or other arithmetic operators
 # Hint, work through binary addition by hand
+def Add(x, y):
+    while (y != 0):
+        carry = x & y
+        x = x ^ y
+        y = carry << 1
+    
+    return x
+
+print(Add(11, 22))
