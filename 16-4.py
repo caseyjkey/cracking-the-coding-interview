c1ddd67ad194c22ec4258dbe46c060d5b0f55956
Casey Key <casey.key@pm.me>
2024-06-11 22:39:12 -0700

    Added solution

--- 16-4.py
+++ 16-4.py
@@ -1,1 +1,10 @@
 # Design an algorithm to decide if someone has won tic-tac-toe
+board = [1, 0, 0, 1, 0, 0, 1, 0, 0]
+
+
+def isWinner(board, player):
+    return any( player == board[a] == board[b] == board[c]
+               for a, b, c in [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7),
+                               (2, 5, 8), (0, 4, 8), (2, 4, 6)])
+
+print(isWinner(board, 1))
