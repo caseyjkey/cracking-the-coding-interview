fd32b15a3c1c6ccbae851db4d1198d6da578817b
Casey Key <casey.key@pm.me>
2024-06-13 21:53:36 -0700

    added solution

--- 16-5.py
+++ 16-5.py
@@ -1,1 +1,11 @@
 # Write a program to find the number of trailing zeros for n factorial
+n = 100
+factorial = 1
+for i in range(2, n + 1):
+    factorial *= i
+factorial = str(factorial)
+answer = 0
+while factorial[-1] == '0':
+    answer += 1
+    factorial = factorial[0:-1]
+print(answer)
