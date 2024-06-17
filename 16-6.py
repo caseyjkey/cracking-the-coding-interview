27603f21eecf33d39ce68a98b303ed9810d3ea0b
Casey Key <casey.key@pm.me>
2024-06-16 22:40:22 -0700

    added solution

--- 16-6.py
+++ 16-6.py
@@ -1,1 +1,14 @@
 # Given two arrays of integers, find the two elements with the smallest difference
+a = [121, 38, 8]
+b = [7, 348, 12]
+
+pair = None
+smallestSum = float('inf')
+
+for i in a:
+    for j in b:
+        if abs(i - j) < smallestSum:
+            pair = (i, j)
+            smallestSum = i - j
+
+print(pair)
