578b7ad0f29aa21e748714323fa79c84ea72c7c8
Casey Key <casey.key@pm.me>
2024-06-16 22:49:52 -0700

    improved solution

--- 16-6.py
+++ 16-6.py
@@ -12,3 +12,23 @@ for i in a:
             smallestSum = i - j
 
 print(pair)
+
+l1 = sorted(a)
+l2 = sorted(b)
+
+i = j = 0
+minimum = float('inf')
+minPair = None
+
+while i < len(l1) and j < len(l2):
+    diff = l1[i] - l2[j]
+    minPair = minPair if abs(diff) > minimum else (l1[i], l2[j])
+    minimum = min(minimum, abs(diff))
+    if diff < 0: # left is smaller than the right
+        i += 1
+    elif diff > 0:
+        j += 1
+    else:
+        break
+
+print(minimum, minPair)
