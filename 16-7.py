206339d4befd3917fcc55da20caae1e0a716988d
Casey Key <casey.key@pm.me>
2024-06-18 21:54:37 -0700

    added solution

--- 16-7.py
+++ 16-7.py
@@ -1,1 +1,7 @@
 # Write a method to find the maximum of two numbers without using if-else or any other comparison operator
+
+def max(a, b):
+    return (a + b) / 2 + abs(a - b) / 2
+
+print(max(5,10))
+print(max(10, 100))
