22967b784d119db9551e6feba9f0668ee1d1006d
Casey Key <casey.key@pm.me>
2024-06-20 22:58:48 -0700

    partial solution

--- 16-8.py
+++ 16-8.py
@@ -1,1 +1,17 @@
 # Given a number, print the English version e.g. One Thousand, Two Hundred Thirty Four
+
+numToWord = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+    }
+number = int(input("Enter a number:"))
+print(numToWord[number])
+
