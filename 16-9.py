06ee991df4673e497f3ccaeceaa5d2f08554583c
Casey Key <casey.key@pm.me>
2024-06-24 23:15:08 -0700

    added initial solution

--- 16-9.py
+++ 16-9.py
@@ -1,2 +1,25 @@
 # Write operations to implement multiply, subtract, and divide operations using only addition. The results
 # are all integers and so are the operands..
+
+
+def mult(a, b):
+    total = 0
+    for i in range(0, b):
+        total += a
+    return total
+
+def div(a, b):
+    total = 0
+    dividend = 0
+    while dividend < a:
+        dividend += b
+        total += 1 if dividend < a else 0
+    return total
+
+def sub(a, b):
+    return a + -b
+
+print("2 * 5", mult(2, 5))
+print("20 / 3", div(20, 3))
+print("5 - 2", sub(5, 2))
+print("-21 / 7", div(-21, 7))
