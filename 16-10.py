c3998877d046afa1afd946da2b3a4870da8ae506
Casey Key <casey.key@pm.me>
2024-06-28 22:38:43 -0700

    added pseudo code

---
+++ 16-10.py
@@ -0,0 +1,6 @@
+# Given a list of peoples birth and death years (between 1900 and 2000)
+# Calculate the year with the most people alive
+
+peopleYears = [(1900, 2000), (1910, 1920), (1905, 1910), (1920, 1930), (1930, 1932)]
+
+# CFor each pair, count how many other pairs are within its range, then find largest
