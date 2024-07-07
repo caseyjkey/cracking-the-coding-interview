4e8e6ba476f20adb349b30992f2bafbaeafe4c30
Casey Key <casey.key@pm.me>
2024-07-06 23:09:33 -0700

    added solution

--- 16-11.py
+++ 16-11.py
@@ -1,4 +1,8 @@
 # You are building a diving board with boards of length "longer" and "shorter"
 # Using K pieces of wood, how many different combinations can be made?
 
-# Tired
+def perms(n: int) -> int:
+    return 2**n
+
+print(perms(3))
+
